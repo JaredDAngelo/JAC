65c1f9645a07115c8859bd03e04127ebfe7904dd
Victor Garcia <vhgm294@gmail.com>
2025-10-01 19:51:51 -0500

    Correcion de errores 3

--- JACback/src/junta/dto/create-junta.dto.ts
+++ JACback/src/junta/dto/create-junta.dto.ts
@@ -1,15 +1,14 @@
 import { Type } from 'class-transformer';
-import { 
-  IsArray, 
-  IsNotEmpty, 
-  IsString, 
-  ValidateNested, 
-  IsOptional 
+import {
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+  IsOptional,
 } from 'class-validator';
 import { ActaDto } from '../../acta/dto/create-acta.dto';
 import { DocumentoDto } from '../../documento/dto/create-documento.dto';
-import { LibrosDto } from 'src/libro/dto/create-libro.dto';
-
+import { LibrosDto } from '../../libro/dto/create-libro.dto';
 
 export class JuntaDto {
   @IsNotEmpty()
